61769c6afb6cffc2b3b078405d957ef3d1f9f65e
林阿三 <1603889221@qq.com>
2024-08-01 01:13:08 +0800

    feat: 新增uni-layouts以及uni-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+    plugins: [UniPages(), UniLayouts(), uni()],
 });
