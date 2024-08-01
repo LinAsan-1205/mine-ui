9853e849af54cd2cec9840584c7d79d2fdd265a5
林阿三 <1603889221@qq.com>
2024-08-01 23:09:13 +0800

    fix: 兼容底部

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -3,10 +3,14 @@ import TabBar from "@/components/layout/tab-bar.vue";
 </script>
 
 <template>
-  <slot/>
-  <tab-bar/>
+  <view class="layout">
+    <slot/>
+    <tab-bar/>
+  </view>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.layout {
+  padding-bottom: calc(env(safe-area-inset-bottom) + 30rpx);
+}
 </style>
